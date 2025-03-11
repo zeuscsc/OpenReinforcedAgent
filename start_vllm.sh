3041e64a650ea9b051a746b07c9dae77943a5109
Zeus Chiu <zeuscsc@gmail.com>
2025-03-11 10:04:45 +0800

    fix: update docker run command in start_vllm.sh for model versioning and memory length adjustment

--- start_vllm.sh
+++ start_vllm.sh
@@ -1,12 +1,12 @@
-docker run -it --name vllm-server --rm --gpus='"device=0"' --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
+docker run -it --name vllm-server-0 --rm --gpus='"device=0"' --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
                  -v $(pwd):/workspace -p 8000:8000 \
                  -e NCCL_P2P_DISABLE=1 -e NCCL_SHM_DISABLE=1 \
                  vllm/vllm-openai:v0.7.2 \
-                 --model /workspace/vllm-models/vllm-7 \
+                 --model /workspace/vllm-models/vllm-0 \
                  --tool-call-parser hermes \
                  --enable-auto-tool-choice \
                  --quantization bitsandbytes \
                  --load-format bitsandbytes \
                  --gpu-memory-utilization 0.8 \
                  --port 8000 \
-                 --max_model_len 8096
+                 --max_model_len 8192
